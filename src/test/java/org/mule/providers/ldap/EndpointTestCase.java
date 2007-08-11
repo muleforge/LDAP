94439e49cd3cc4e92ddf5f6a39f5ae7a80a6286b
hsaly <hsaly@8ffcf14e-6d36-0410-bafe-9177b3f51c58>
2007-08-11 08:04:34 +0000

    new testmethods and some debug messages added

--- src/test/java/org/mule/providers/ldap/EndpointTestCase.java
+++ src/test/java/org/mule/providers/ldap/EndpointTestCase.java
@@ -72,7 +72,7 @@ public class EndpointTestCase extends AbstractMuleTestCase
         }
 
     }
-    
+
     public void testBadParamatersWithDollarSign() throws Exception
     {
 
@@ -105,104 +105,126 @@ public class EndpointTestCase extends AbstractMuleTestCase
         assertEquals("ldap://ldap.out/payload.dc", url.toString());
         assertEquals("/payload.dc", url.getPath());
     }
-    
+
     public void testSearchStringFromEndpoint1() throws Exception
     {
         UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/(cn=*)");
         UMOConnector connector = getConnector();
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,null);
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                null);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("(cn=*)", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpoint2() throws Exception
     {
         UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/payload.cn");
         UMOConnector connector = getConnector();
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,null);
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                null);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("(cn=test)", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpoint3() throws Exception
     {
         UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/");
         UMOConnector connector = getConnector();
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,null);
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                null);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("test", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpoint4() throws Exception
     {
         UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out");
         UMOConnector connector = getConnector();
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,null);
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                null);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("test", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpoint5() throws Exception
     {
-        UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/payload.noquery");
+        UMOEndpointURI url = new MuleEndpointURI(
+                "ldap://ldap.out/payload.noquery");
         UMOConnector connector = getConnector();
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,null);
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                null);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("payload.noquery", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpointOverrideProps() throws Exception
     {
         UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/payload.cn");
         UMOConnector connector = getConnector();
-        
+
         Map map = new HashMap();
         Map inner = new HashMap();
         inner.put("payload.cn", "(objectclass=${payload})");
         map.put("queries", inner);
-        
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,map);
-        
-        
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, "test");
-        
+
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                map);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                "test");
+
         assertEquals("(objectclass=test)", searchStr);
-        
+
     }
-    
+
     public void testSearchStringFromEndpointOverrideProps2() throws Exception
     {
-        UMOEndpointURI url = new MuleEndpointURI("ldap://ldap.out/payload.cn?cnprop=testprop");
+        UMOEndpointURI url = new MuleEndpointURI(
+                "ldap://ldap.out/payload.cn?cnprop=testprop");
         UMOConnector connector = getConnector();
-        
+
         Map map = new HashMap();
         Map inner = new HashMap();
         inner.put("payload.cn", "(sn=${name}, cn=${cnprop})");
         map.put("queries", inner);
-        
-        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,null,UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER,0,null,map);
-                
-        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint, new User("john"));
-        
+
+        UMOEndpoint endpoint = new MuleEndpoint("testendpoint", url, connector,
+                null, UMOEndpoint.ENDPOINT_TYPE_SENDER_AND_RECEIVER, 0, null,
+                map);
+
+        String searchStr = LDAPUtils.getSearchStringFromEndpoint(endpoint,
+                new User("john"));
+
         assertEquals("(sn=john, cn=testprop)", searchStr);
-        
+
     }
-    
+
     public UMOConnector getConnector() throws Exception
     {
 
@@ -215,17 +237,17 @@ public class EndpointTestCase extends AbstractMuleTestCase
         c.setPassword("secret");
 
         c.setSearchBase("o=sevenSeas");
-        
+
         Map map = new HashMap();
         map.put("payload.cn", "(cn=${payload})");
-        
+
         c.setQueries(map);
-        
+
         c.initialise();
 
         return c;
     }
-    
+
     public static final class User
     {
         private String name = "default";
@@ -234,7 +256,7 @@ public class EndpointTestCase extends AbstractMuleTestCase
         {
             setName(name);
         }
-        
+
         public String getName()
         {
             return name;
